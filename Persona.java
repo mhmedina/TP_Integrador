0bc013a9e09a4625966c94bb79e3a3dbad7176dc
mhmedina <mhmedinaa@yahoo.es>
2023-03-26 21:37:01 -0300

    Esta es una segunda prueba

--- Persona.java
+++ Persona.java
@@ -1,5 +1,11 @@
 package pronosticos_trabajo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,9 +19,11 @@ public class Persona {
 
 		pronosticos.add(p1);
 		}
-	
 
 	public static void main(String[] args) {
+		
+	  
+	    
 		List<Pronostico> pronosticos = new ArrayList<>();
 		pronosticos.add(new Pronostico("Qatar", "Ecuador", 0, 2));
 	    
@@ -25,15 +33,19 @@ public class Persona {
 		Pronostico pronosticoMaria = personas.get(0).pronosticos.get(0); // obtiene el primer pronóstico de la primera persona de la lista de personas
 
 		if (pronosticoMaria.getGolesLocal() == 0 && pronosticoMaria.getGolesVisitante() == 2 && pronosticoMaria.getEquipoLocal().equals("Qatar") && pronosticoMaria.getEquipoVisitante().equals("Ecuador")) {
-		    System.out.println("María Pérez acertó el resultado del partido Qatar vs Ecuador");
-		} else {
-		    System.out.println("María Pérez no acertó el resultado del partido Qatar vs Ecuador");
+		    System.out.println("Maria Perez acerto el resultado del partido Qatar vs Ecuador");
+		} 
+		
+		else {
+		    System.out.println("Maria Perez no acerto el resultado del partido Qatar vs Ecuador");
 		}
+	    }
+	}
 
 	
 
-	}
-}
+	
+
 
 
 
